1b078d6cb548c0ce1ec19af3b8d9480e660c2ef1
rishitech-ai <your_rishitech_email@example.com>
2026-02-04 10:51:32 +0530

    Day 14 & 15

---
+++ script.js
@@ -0,0 +1,17 @@
+
+console.log("loaded");
+
+let name = "Rishabh";
+let age = 22;
+let isStudent = true;
+let skills = ["HTML", "CSS"];
+let profile = {
+  city: "Delhi",
+  course: "JavaScript"
+};
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Skills:", skills);
+console.log("Profile:", profile);
