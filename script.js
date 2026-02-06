c9397ff9288e77001d74407eee0bcdecf6c9927c
rishitech-ai <your_rishitech_email@example.com>
2026-02-06 11:02:50 +0530

    Day 17

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-console.log("loaded");
+// console.log("loaded");
 
-let name = "Rishabh";
-let age = 22;
-let isStudent = true;
-let skills = ["HTML", "CSS"];
-let profile = {
-  city: "Delhi",
-  course: "JavaScript"
-};
+// let name = "Rishabh";
+// let age = 22;
+// let isStudent = true;
+// let skills = ["HTML", "CSS"];
+// let profile = {
+//   city: "Delhi",
+//   course: "JavaScript"
+// };
 
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("Is Student:", isStudent);
-console.log("Skills:", skills);
-console.log("Profile:", profile);
+// console.log("Name:", name);
+// console.log("Age:", age);
+// console.log("Is Student:", isStudent);
+// console.log("Skills:", skills);
+// console.log("Profile:", profile);
 
 // 1. greet function
 function greet(name) {
@@ -39,3 +39,18 @@ console.log("Fahrenheit:",toFahrenheit(25));
 
 
 
+let age = prompt("Enter your age");
+
+if (age === "" || isNaN(age)) {
+  alert("Invalid age");
+} else {
+  age = Number(age);
+
+  if (age < 18) {
+    alert("Minor");
+  } else if (age >= 18 && age <= 60) {
+    alert("Adult");
+  } else {
+    alert("Senior");
+  }
+}
